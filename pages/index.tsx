6c70bafa1bebaf49f21fdad4bca7b0c322064d48
SulfurBottom <397love@gmail.com>
2022-04-28 16:49:35 +0900

    tailwind css 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,122 +1,63 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>BarBamBar</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Exo:wght@900&family=Poppins:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
-      <div id="main">
-        <div id="menu_bar">
-          <Image src="/img/cat.png" width={32} height={32} alt="logo" />
-          <div id="title">BarBamBar</div>
-          <div id="tab_container">
-            <div className="tab">ABOUT</div>
-            <div className="tab">MENU</div>
-            <div className="tab">HERO</div>
+      <nav className="flex items-center justify-between flex-wrap bg-black p-4">
+        <div className="flex items-center flex-shrink-0 text-white mr-20">
+          <Image src="/img/cat.png" width={24} height={24} />
+          <span className="font-exo font-extrabold text-2xl tracking-tight ml-3">
+            BarBamBar
+          </span>
+        </div>
+
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="font-exo font-semibold block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4"
+            >
+              ABOUT
+            </a>
+            <a
+              href="#responsive-header"
+              className="font-exo font-semibold block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4"
+            >
+              MENU
+            </a>
+            <a
+              href="#responsive-header"
+              className="font-exo font-semibold block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4"
+            >
+              HEROS
+            </a>
           </div>
-          <div id="export_btn_container">
-            <Image
-              src={"/img/youtube.png"}
-              width={30}
-              height={30}
-              alt="youtube"
-              onClick={() => {
-                window.open(
-                  "https://www.youtube.com/channel/UCzww46F6Cjj_I751OtLSXSQ"
-                );
-              }}
-              style={{ cursor: "pointer" }}
-            />
-            <Image
-              src="/img/instagram.png"
-              width={25}
-              height={25}
-              alt="instagram"
-              onClick={() => {
-                window.open("https://www.instagram.com/yosajangtv/");
-              }}
-              style={{ cursor: "pointer" }}
-            />
+          <div className="flex mt-3 lg:mt-0">
+            <Link href="https://www.youtube.com/channel/UCzww46F6Cjj_I751OtLSXSQ">
+              <a
+                className="cursor-pointer m-2 items-center flex"
+                target={`_blank`}
+              >
+                <Image src={`/img/youtube.png`} width={24} height={24} />
+              </a>
+            </Link>
+            <Link href="https://www.instagram.com/yosajangtv/">
+              <a
+                className="cursor-pointer m-2 items-center flex"
+                target={"_blank"}
+              >
+                <Image src={`/img/instagram.png`} width={24} height={24} />
+              </a>
+            </Link>
           </div>
         </div>
-        <div id="body">BarBamBar</div>
-      </div>
-
-      <style jsx>
-        {`
-          div {
-            display: flex;
-            flex-direction: column;
-          }
-          .tab {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            color: #fff;
-            font-family: "Exo", sans-serif;
-            font-weight: bold;
-            cursor: pointer;
-          }
-          #main {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-          }
-          #menu_bar {
-            background-color: #000;
-            width: 100%;
-            height: 50px;
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            padding-left: 10px;
-          }
-          #body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            width: 100%;
-            height: 100%;
-          }
-          #title {
-            font-size: 1.6em;
-            font-family: "Exo", sans-serif;
-            font-weight: 900;
-            color: #fff;
-            margin-left: 10px;
-          }
-          #export_btn_container {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            justify-content: center;
-            margin-left: auto;
-            margin-right: 10px;
-            gap: 1em;
-          }
-          #tab_container {
-            display: flex;
-            flex-direction: row;
-            flex: stretch;
-            justify-content: space-between;
-            margin: 0 auto;
-            width: 100%;
-            padding: 0 10em;
-          }
-        `}
-      </style>
+      </nav>
     </div>
   );
 };
