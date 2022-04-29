60bd23015a7d3dd0a68682807079b18c80dbb37f
SulfurBottom <397love@gmail.com>
2022-04-29 14:02:48 +0900

    Landing page 분리

---
+++ pages/landing.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Landing: NextPage = () => {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="relative flex h-full flex-col items-center justify-center">
+        <Image src="/img/main.jpg" layout="fill" objectFit="cover" />
+        <div className="absolute text-center flex flex-col">
+          <div className="lg:mb-10 m-5">
+            <span className="bg-black pl-5 pr-5 font-exo lg:text-8xl text-5xl  text-white">
+              BarBamBar
+            </span>
+          </div>
+          <div>
+            <span className="bg-black pr-3 pl-3 pt-3 pb-2 font-kor lg:text-6xl text-3xl  text-white">
+              고양이와 함께하는 바
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="h-1/6 flex flex-col justify-center align-middle text-center font-kor bg-black text-white">
+        <span className="text-xl">
+          서울시 영등포구 영중로4길 25-1(영등포동3가 23-8) 2층
+        </span>
+        <span>이 사이트는 바밤바 공식 사이트가 아닌, 팬 사이트입니다</span>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
