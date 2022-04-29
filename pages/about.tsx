e949a1b3d7990f55363673edb616a5dfe4d1b406
SulfurBottom <397love@gmail.com>
2022-04-29 16:33:04 +0900

    about 중간 저장

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const About: NextPage = () => {
-  return <>about</>;
+  return (
+    <div className="flex justify-center">
+      <Head>
+        <title>About</title>
+      </Head>
+      <div className="container  mt-5">
+        <div className="text-center">
+          <div>
+            <div className="font-exo text-3xl">Who is Yosajang?</div>
+            <Image
+              src="/img/yosi_heroine.jpg"
+              height={300}
+              width={300}
+              objectFit="cover"
+            />
+          </div>
+          <div className="flex flex-col">
+            <div className="font-exo text-xl">I want to be friends</div>
+            <div className="font-exo text-2xl">with you.</div>
+            <div className="">영등포 바밤바의 사장</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
