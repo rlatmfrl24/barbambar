0d0fe3fb61a27a77ddf60869b8f482ada12dcd8e
SulfurBottom <397love@gmail.com>
2022-05-17 23:19:40 +0900

    contact page 완성

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,24 +1,99 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
+
+type ContactData = {
+  title: string;
+  imgpath: string;
+  link: string;
+};
+
+const ContactButton: NextPage<ContactData> = ({ title, imgpath, link }) => {
+  return (
+    <Link href={link} passHref>
+      <a
+        className="flex items-center py-2 px-5 font-exo lg:text-2xl text-lg gap-2 bg-white shadow-md rounded-xl"
+        target={`_blank`}
+      >
+        <Image
+          src={imgpath}
+          width={32}
+          height={32}
+          layout="intrinsic"
+          objectFit="cover"
+        />
+        {title}
+      </a>
+    </Link>
+  );
+};
 
 const Contact: NextPage = () => {
+  const [showPhoneNum, setShowPhoneNum] = useState(false);
+  useEffect(() => {
+    setShowPhoneNum(isMobile);
+  }, [isMobile]);
+
   return (
-    <div className="flex justify-center bg-gray-100 h-full">
+    <div className="flex flex-col justify-center items-center bg-gray-100 h-full">
       <Head>
         <title>Contact</title>
       </Head>
-      <div className="container">
-        <div className="flex justify-center">
-          <div>
-            <Image src="/img/map.svg" alt="map" height={100} width={100} />
-            Location
-          </div>
-          <div>Phone</div>
-          <div>Email</div>
-          <div>Youtube</div>
-          <div>Instagram</div>
+      <div className="flex flex-col flex-wrap gap-3">
+        <div className="flex flex-col justify-center items-start font-exo">
+          <p className="lg:text-7xl md:text-6xl sm:text-5xl text-4xl">
+            Contact to
+          </p>
+          <p className="lg:text-9xl md:text-8xl sm:text-7xl text-6xl">
+            BarBamBar
+          </p>
         </div>
+        <div className="border-2 border-black"></div>
+        <div className="flex md:flex-row flex-col justify-center gap-3 mt-3">
+          <ContactButton
+            title="Location"
+            imgpath="/img/map.svg"
+            link="https://place.map.kakao.com/14576513"
+          />
+          {showPhoneNum && (
+            <ContactButton
+              title="Call"
+              imgpath="/img/call.svg"
+              link="tel:010-5245-5245"
+            />
+          )}
+
+          <ContactButton
+            title="Youtube"
+            imgpath="/img/youtubetv.svg"
+            link="https://www.youtube.com/channel/UCzww46F6Cjj_I751OtLSXSQ"
+          />
+          <ContactButton
+            title="Instagram"
+            imgpath="/img/instagram_black.png"
+            link="https://www.instagram.com/yosajangtv/"
+          />
+        </div>
+      </div>
+      <div>
+        {!showPhoneNum && (
+          <p
+            id="phone_num_for_pc"
+            className="flex justify-center items-center gap-2 mt-10 font-exo text-2xl"
+          >
+            <Image
+              src="/img/call.svg"
+              width={24}
+              height={24}
+              layout="intrinsic"
+              objectFit="cover"
+            />
+            <span>02-835-5487</span>
+          </p>
+        )}
       </div>
     </div>
   );
