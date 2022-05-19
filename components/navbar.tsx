b2a68b3230bafd235afc7be4cd3e0e414d6b4315
SulfurBottom <397love@gmail.com>
2022-05-19 10:17:59 +0900

    기존 동작 로직 롤백 랜딩페이지 주소 부분 마진 추가

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,16 +6,12 @@ import { useEffect, useState } from "react";
 
 const NavBar: NextPage = () => {
   const [isClose, setIsClose] = useState(false);
-  const [currentWidth, setCurrentWidth] = useState(0);
   const toggle = () => setIsClose(!isClose);
   const handleResize = () => {
-    setCurrentWidth(window.innerWidth);
-    if (currentWidth !== window.innerWidth) {
-      if (window.innerWidth > 768 && currentWidth !== window.innerWidth) {
-        setIsClose(true);
-      } else {
-        setIsClose(false);
-      }
+    if (window.innerWidth > 768) {
+      setIsClose(true);
+    } else {
+      setIsClose(false);
     }
   };
 
